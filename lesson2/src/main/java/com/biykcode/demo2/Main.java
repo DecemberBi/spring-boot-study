051f7a6978d1d6b4840d9f9e4b663928c5689a7a
biyukun <1431329678@qq.com>
2021-11-30 23:02:23 +0800

    'lesson2'

---
+++ lesson2/src/main/java/com/biykcode/demo2/Main.java
@@ -0,0 +1,13 @@
+package com.biykcode.demo2;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ElConfig.class);
+        ElConfig resourceService = context.getBean(ElConfig.class);
+        resourceService.outputResource();
+        context.close();
+    }
+}
