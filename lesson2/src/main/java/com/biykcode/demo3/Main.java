9a6f6aff714b5aa2b2bedcbb31d17a01d5bae6cb
biyukun <1431329678@qq.com>
2021-11-30 23:14:49 +0800

    'demo3'

---
+++ lesson2/src/main/java/com/biykcode/demo3/Main.java
@@ -0,0 +1,16 @@
+package com.biykcode.demo3;
+
+import com.biykcode.demo3.prepost.BeanWayService;
+import com.biykcode.demo3.prepost.JSR250WayService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(PrePostConfig.class);
+        BeanWayService beanWayService = context.getBean(BeanWayService.class);
+        JSR250WayService jsr250WayService = context.getBean(JSR250WayService.class);
+
+        context.close();
+    }
+}
---
+++ lesson2/src/main/java/com/biykcode/demo3/PrePostConfig.java
@@ -0,0 +1,22 @@
+package com.biykcode.demo3;
+
+import com.biykcode.demo3.prepost.BeanWayService;
+import com.biykcode.demo3.prepost.JSR250WayService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.biykcode.demo3.prepost")
+public class PrePostConfig {
+
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    BeanWayService beanWayService() {
+        return new BeanWayService();
+    }
+
+    @Bean
+    JSR250WayService jsr250WayService() {
+        return new JSR250WayService();
+    }
+}
